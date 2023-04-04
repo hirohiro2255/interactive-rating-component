6cfe5f577a0fd1a41cfa68343e41184fb65cc3ea
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2023-04-04 21:05:35 +0900

    Break app into some components

---
+++ src/components/MainCard.svelte
@@ -0,0 +1,95 @@
+<script>
+  import iconStar from './../images/icon-star.svg';
+</script>
+
+<div class="input-card-group">
+  <section class="img-container">
+    <div>
+      <img class="star" src={iconStar} alt="star-icon" />
+    </div>
+  </section>
+  <section class="description-group">
+    <h1 class="heading-title">How did we do?</h1>
+    <p class="paragraph">
+      Please let us know how we did with your support request. All feedback is
+      appreciated to help us improve our offering!
+    </p>
+  </section>
+  <section class="button-container">
+    <button class="button">1</button>
+    <button class="button">2</button>
+    <button class="button">3</button>
+    <button class="button">4</button>
+    <button class="button">5</button>
+  </section>
+  <button class="submit-button">SUBMIT</button>
+</div>
+
+<style>
+  .input-card-group {
+    margin-top: 24px;
+    margin-bottom: 32px;
+    margin-right: 22px;
+    margin-left: 22px;
+  }
+  .img-container div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--neutral-dark-blue);
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+  }
+  .star {
+    width: 13.96px;
+    height: 13.33px;
+  }
+  .description-group {
+    margin-top: 16px;
+  }
+  .heading-title {
+    color: var(--neutral-white);
+    font-size: 1.5rem;
+    font-weight: 700;
+    line-height: 1.25;
+  }
+  .paragraph {
+    color: var(--neutral-light-grey);
+    font-size: 0.875rem;
+    line-height: 1.57;
+    margin-top: 17px;
+  }
+  .button-container {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 24px;
+  }
+  .button {
+    width: 42px;
+    height: 42px;
+    border-radius: 50%;
+    background-color: var(--neutral-dark-blue);
+    border-style: none;
+    color: var(--neutral-medium-grey);
+    font-weight: 700;
+    font-size: 0.875rem;
+    line-height: 1.7;
+    text-align: center;
+  }
+
+  .submit-button {
+    margin-top: 24px;
+    width: 100%;
+    padding-top: 14px;
+    padding-bottom: 14px;
+    border-style: none;
+    border-radius: 22.5px;
+    background-color: var(--primary-orange);
+    color: var(--neutral-white);
+    font-weight: 700;
+    font-size: 0.875px;
+    line-height: 1.28;
+    letter-spacing: 1.87px;
+  }
+</style>
---
+++ src/components/SubmittedCard.svelte
@@ -0,0 +1,63 @@
+<script>
+  import thankYouIllustration from './../images/illustration-thank-you.svg';
+</script>
+
+<div class="submitted-group">
+  <section class="img-container">
+    <img class="payment-img" src={thankYouIllustration} alt="thank-you-img" />
+  </section>
+  <p class="result-paragraph">You selected 4 out of 5</p>
+  <section class="description-group">
+    <h1>Thank you!</h1>
+    <p>
+      We appreciate you taking the time to give a rating. If you ever need more
+      support, don’t hesitate to get in touch!
+    </p>
+  </section>
+</div>
+
+<style>
+  .submitted-group {
+    margin-top: 34px;
+    margin-bottom: 37px;
+    margin-right: 24px;
+    margin-left: 24px;
+  }
+  .payment-img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .result-paragraph {
+    margin-top: 24px;
+    background-color: var(--neutral-dark-blue);
+    color: var(--primary-orange);
+    width: 168px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-left: 12px;
+    padding-right: 12px;
+    margin-left: auto;
+    margin-right: auto;
+    font-size: 0.875rem;
+    line-height: 1.57;
+    border-radius: 22.5px;
+  }
+  .description-group {
+    margin-top: 24px;
+  }
+  .description-group h1 {
+    color: var(--neutral-white);
+    font-weight: 700;
+    font-size: 1.5rem;
+    line-height: 1.25;
+    text-align: center;
+  }
+  .description-group p {
+    margin-top: 17px;
+    color: var(--neutral-light-grey);
+    font-size: 0.875rem;
+    line-height: 1.57;
+    text-align: center;
+  }
+</style>
